c6d9dbfbea2ff61d7772c11a8aff72d7b4d02e7d
Kaung Set <kset2299@gmail.com>
2025-02-28 19:56:08 +0630

    refactor ray struct and enhance ray_color function for sphere intersection

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use color::{Color, write_color};
+use color::{write_color, Color};
 use ray::Ray;
-use vec3::{Point3, Vec3};
+use vec3::{dot, Point3, Vec3};
 
 pub mod color;
 pub mod ray;
 pub mod vec3;
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = center - *r.origin();
+    let a = dot(*r.direction(), *r.direction());
+    let b = -2.0 * dot(*r.direction(), oc);
+    let c = dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.direction();
     let t = 0.5 * (unit_direction.y + 1.0);
-    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+    return Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t;
 }
 
 fn main() {
@@ -29,7 +42,7 @@ fn main() {
 
     // calculate the horizontal and vertical vectors
     let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, viewport_height, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     // calculate the horizontal and vertical deltas
     let pixel_delta_u = viewport_u / (image_width as f64);
@@ -38,12 +51,12 @@ fn main() {
     // calculate the location of upper left pixel
     let viewport_upper_left =
         camera_center - (viewport_u / 2.0) - (viewport_v / 2.0) - Vec3::new(0.0, 0.0, focal_length);
-    let pixel100_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5; 
+    let pixel100_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
     // Render
     println!("P3\n{} {}\n255", image_width, image_height);
 
-    for j in (0..image_height).rev() {
+    for j in 0..image_height {
         eprintln!("\rScan lines remaining: {} ", image_height - j);
         for i in 0..image_width {
             let pixel_center =
@@ -52,7 +65,7 @@ fn main() {
             let r = Ray::new(camera_center, ray_direction);
 
             let pixel_color = ray_color(r);
-            write_color(pixel_color, |s| print!("{}", s));
+            write_color(pixel_color, |s| println!("{}", s));
         }
     }
 
--- src/ray.rs
+++ src/ray.rs
@@ -1,17 +1,3 @@
-// class ray {
-//     public:
-//     ray() {}
-//     ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
-//     const point3& origin() const { return orig; }
-//     const vec3& direction() const { return dir; }
-//     point3 at(double t) const {
-//     return orig + t*dir;
-//     }
-//     private:
-//     point3 orig;
-//     vec3 dir;
-//     };
-
 use crate::vec3::{Point3, Vec3};
 
 pub struct Ray {
